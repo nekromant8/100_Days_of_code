eeccc99077743c0055e04f2b18c215b7f8be296c
Sergey Stankevich <nekromant8@gmail.com>
2022-08-01 10:48:46 +0300

    ADDED: High_score support

--- snake_game/main.py
+++ snake_game/main.py
@@ -31,13 +31,13 @@ while game_is_on:
         snake.extend()
     # Detect collision with the wall
     if snake.head.xcor() > 295 or snake.head.xcor() < -300 or snake.head.ycor() > 295 or snake.head.ycor() < -300:
-        scoreboard.game_over()
-        game_is_on = False
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -2,21 +2,38 @@ from turtle import Turtle
 ALIGNMENT = 'center'
 FONT = ('Courier', 14, 'normal')
 
+
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score_num = 0
+        self.high_score = int(self.check_high_score())
         self.hideturtle()
         self.penup()
         self.color('white')
         self.goto(x=0, y=280)
-        self.write(f'Score: {self.score_num}', move=False, align=ALIGNMENT, font=FONT)
+        self.write(f'Score: {self.score_num} High Score: {self.high_score}', move=False, align=ALIGNMENT, font=FONT)
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f'Score: {self.score_num} High Score: {self.high_score}', move=False, align=ALIGNMENT, font=FONT)
+
+    def check_high_score(self):
+        with open('data.txt', "r") as f:
+            data = f.read()
+            return data
+
+    def update_high_score(self):
+        with open('data.txt', "w") as f:
+            f.write(str(self.score_num))
+
+    def reset(self):
+        if self.score_num > self.high_score:
+            self.high_score = self.score_num
+            self.update_high_score()
+        self.score_num = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score_num += 1
-        self.clear()
-        self.write(f'Score: {self.score_num}', move=False, align=ALIGNMENT, font=FONT)
-
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f'Game over.', move=False, align=ALIGNMENT, font=FONT)
+        self.update_scoreboard()
